97a8f8922d1ff56fd06179f4062267c18e31033c
Umar Lodhi <umarlodhi86@gmail.com>
2017-12-09 17:08:54 +0500

    Add projects page

--- server.js
+++ server.js
@@ -50,6 +50,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+// /projects
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
 // /bad - send back json with errorMessage
 
 app.get('/bad', (req, res) => {
