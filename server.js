3fb1629ef5a026fe0229f2b770fbc911a2726364
Umar Lodhi <umarlodhi86@gmail.com>
2017-12-09 15:12:50 +0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+// middleware
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  //res.send('<h1>Hi Express!</h1>');
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to My Website',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+// /bad - send back json with errorMessage
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Error Occured Something Went Wrong!!!'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Running server on port 3000...');
+});
